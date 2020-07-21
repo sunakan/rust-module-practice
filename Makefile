d807ff085e69752569c889a702333b15deecabb0
sunakan <sunakan03@gmail.com>
2020-07-21 11:20:51 +0000

    Initial commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+include shared-makefiles/gitignore.mk
+include shared-makefiles/help.mk
---
+++ shared-makefiles/rust.mk
@@ -0,0 +1,22 @@
+################################################################################
+# タスク
+################################################################################
+.PHONY: run
+run: ## cargo run
+	cargo run
+
+.PHONY: fmt
+fmt: ## cargo fmt
+	cargo fmt
+
+.PHONY: check
+check: ## cargo check
+	cargo check
+
+.PHONY: test
+test: ## cargo test
+	cargo test
+
+.PHONY: clean
+clean: ## 片付け
+	rm -rf target
